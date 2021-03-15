f3f99a31dd9c5983ae652493a9091e51a4de86c4
wei chun lin <virginial2123@gmail.com>
2021-03-15 11:03:12 +0800

    add: set copy webpack plugin

--- webpack.prod.babel.js
+++ webpack.prod.babel.js
@@ -1,12 +1,20 @@
+import path from 'path';
 import { merge } from 'webpack-merge';
 import baseWebpackConfig from './webpack.base.babel.js';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 export default merge ( baseWebpackConfig, {
   stats: 'errors-only',
   mode: 'production',
   devtool: 'eval',
   plugins: [
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns:[
+        { from: 'static', to: 'static' },
+        { from: path.resolve(__dirname, 'php'), to: 'php', noErrorOnMissing: true}
+      ]
+    })
   ]
 });
