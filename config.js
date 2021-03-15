d64808262fb98e35513382a233b11b07c0df0189
wei chun lin <virginial2123@gmail.com>
2021-03-15 11:01:07 +0800

    edit: add url

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const DEV_ENV = process.env.DEV_ENV;
 const PROD_ENV = process.env.PROD_ENV;
 const NODE_ENV = process.env.NODE_ENV;
 
-const HOST = ''; //domain
-const PUBLICPATH = '';
+const HOST = '//lab-event.udn.com'; //domain
+const PUBLICPATH = '/act2021_dodo/webpack_test';
 const PORT = 8080;
 
 export {
