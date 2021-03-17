21967521c814247602303dee8d1a17d7350a9730
wei chun lin <virginial2123@gmail.com>
2021-03-17 15:13:03 +0800

    add: add prefix by autoprefixer

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: {
+    "postcss-preset-env": {
+      "autoprefixer": {
+        grid: true,
+        overrideBrowserslist: [
+          '> 1%',
+          'last 5 versions',
+          'Firefox >= 45',
+          'ios >= 8',
+          'ie >= 10'
+        ]
+      }
+    }
+  }
+};
